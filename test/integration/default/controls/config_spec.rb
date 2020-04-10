50de5b91dca94bc2d665e4eb6467bb3fbdf2e1b4
Imran Iqbal <iqbalmy@hotmail.com>
2020-04-10 18:44:46 +0100

    test(config_spec): test bug fixed by #33

--- test/integration/default/controls/config_spec.rb
+++ test/integration/default/controls/config_spec.rb
@@ -35,21 +35,29 @@ end
 control 'openldap server configuration' do
   title 'should match desired lines'
 
-  server_config, owned_and_grouped =
-    case platform[:family]
-    when 'debian'
-      %w[/etc/ldap/slapd.conf openldap]
-    when 'bsd'
-      %w[/usr/local/etc/openldap/slapd.conf ldap]
-    else
-      %w[/etc/openldap/slapd.conf ldap]
-    end
+  server_config = '/etc/openldap/slapd.conf'
+  owned_and_grouped = 'ldap'
+  modulepath = '/usr/lib64/openldap'
+  moduleload = ''
+  case platform[:family]
+  when 'debian'
+    server_config = '/etc/ldap/slapd.conf'
+    owned_and_grouped = 'openldap'
+    modulepath = '/usr/lib/ldap'
+    moduleload = 'back_hdb'
+  when 'bsd'
+    server_config = '/usr/local/etc/openldap/slapd.conf'
+    modulepath = '/usr/local/libexec/openldap'
+  end
+  slap_modules = "modulepath	#{modulepath}"
+  slap_modules += "\nmoduleload      #{moduleload}" unless moduleload.empty?
 
   describe file(server_config) do
     it { should be_file }
     it { should be_owned_by owned_and_grouped }
     it { should be_grouped_into owned_and_grouped }
     its('mode') { should cmp '0644' }
+    its('content') { should include slap_modules }
     its('content') { should include 'rootdn		"cn=root,dc=example,dc=com"' }
     its('content') { should include 'rootpw		{SSHA}5++yqs7UNz22kAYf7jboAmklhavVXahK' }
     its('content') { should include 'index objectClass eq' }
